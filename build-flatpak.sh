b74feae8e2b5421109d151cfdf850842a2643f87
11bdev <226638621+11bDev@users.noreply.github.com>
2025-12-06 17:54:58 -0600

    v0.3.2 - Fix install/update buttons

--- build-flatpak.sh
+++ build-flatpak.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Build script for Autonomix Flatpak package
-VERSION="0.3.1"
+VERSION="0.3.2"
 APP_ID="io.github.plebone.autonomix"
 
 echo "Building Autonomix Flatpak v${VERSION}..."
--- build-rpm.sh
+++ build-rpm.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Build script for Autonomix RPM package
-VERSION="0.3.1"
+VERSION="0.3.2"
 RELEASE="1"
 ARCH="x86_64"
 
